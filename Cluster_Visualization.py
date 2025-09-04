7ac8b91eb94d8d37a2ab65337d2a0690665d79d6
Pranav9805 <pranavvenki9805@gmail.com>
2025-09-04 23:35:38 +0530

    Update Cluster_Visualization.py

--- Cluster_Visualization.py
+++ Cluster_Visualization.py
@@ -2,18 +2,14 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Load clustered data
 df = pd.read_csv('results/clustered_customers.csv')
 
-# Boxplot of Age distribution by Cluster
 plt.figure(figsize=(8,5))
 sns.boxplot(x='Cluster', y='Age', data=df, palette='Set2')
 plt.title('Age Distribution by Cluster')
 plt.xlabel('Cluster')
 plt.ylabel('Age')
 plt.show()
-
-# Countplot for Gender distribution by Cluster
 plt.figure(figsize=(8,5))
 sns.countplot(x='Cluster', hue='Gender', data=df, palette='Set1')
 plt.title('Gender Distribution by Cluster')
